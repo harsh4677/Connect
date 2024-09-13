7e6db048940330c35b2f8b96d2f8ae6eeda58b9a
harsh4677 <harshsantoshsalve@gmail.com>
2024-09-13 14:10:34 +0530

    Created HomePage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+module.exports = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+      return config;
+    },
+  };
+  
