ae1002ae920c7919c8c75f853c792a0ee42ff300
harsh4677 <harshsantoshsalve@gmail.com>
2024-09-15 03:47:35 +0530

    final changes

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -7,14 +7,14 @@ interface HomeCardProps {
     img: string,
     title: string,
     description: string,
-    handleClick: () => void;  // Corrected to "handleClick"
+    handleClick: () => void;  
 }
 
-const HomeCard = ({ className, img, title, description, handleClick }: HomeCardProps) => {  // Updated to "handleClick"
+const HomeCard = ({ className, img, title, description, handleClick }: HomeCardProps) => { 
     return (
         <div
             className={cn('px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer', className)}
-            onClick={handleClick}  // Updated to "handleClick"
+            onClick={handleClick}  
         >
             <div className='flex-center glassmorphism size-12 rounded-[10px]'>
                 <Image
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Image from 'next/image'
 import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
