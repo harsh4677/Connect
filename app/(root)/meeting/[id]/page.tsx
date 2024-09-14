277b930aedab23565e2ab5bcdc7d40a921551f53
harsh4677 <harshsantoshsalve@gmail.com>
2024-09-15 04:13:12 +0530

    changes

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -10,7 +10,7 @@ import React, { useState } from 'react'
 
 const Meeting = ({ params: { id } }: { params: { id : string } })=> {
 
-  const {user, isLoaded } = useUser();
+  const {isLoaded } = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
   const {call, isCallLoading} = useGetCallById(id);
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
